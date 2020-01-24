f718ee959ecbb86d2c424dbc61faf2b0a10d359b
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2020-01-24 19:59:34 +0000

    added ack host discovery

--- source/host_discovery.py
+++ source/host_discovery.py
@@ -4,6 +4,22 @@ import scapy.all as scapy
 # Local Includes
 from arguments import args
 
+def ackping(IP,ports,verbosity=0):
+    print("[-] Running ACK host discovery on port(s) {}".format(ports))
+    
+    p = scapy.IP(dst=IP)/scapy.TCP(dport=ports,flags='A')
+    answered, unanswered= scapy.sr(p,timeout=0.2,retry=1,verbose=verbosity)
+
+    for sent,received in answered:
+        if received.haslayer('TCP'):
+            tcp=received.getlayer('TCP')
+            print(
+            "[+] Host found on port: {} - port unfiltered".format(tcp.sport)
+            )
+            return True
+
+    return False
+
 
 def synping(IP,ports,verbosity=0):
     print("[-] Running SYN/half-open host discovery on port(s) {}".format(ports))
@@ -11,21 +27,22 @@ def synping(IP,ports,verbosity=0):
     p = scapy.IP(dst=IP)/scapy.TCP(dport=ports,flags='S')
     answered, unanswered= scapy.sr(p,timeout=0.2,retry=1,verbose=verbosity)
     
-    for sent, recieved in answered:
-        tcp_layer=recieved.getlayer('TCP')
+    for sent, received in answered:
+        tcp_layer=received.getlayer('TCP')
+
         if tcp_layer.flags.value == 0x12:
             if verbosity == True:
-                print("[+] Host found on port: ", tcp_layer.sport,"port open")
-            found_host=True
+                print(
+                    "[+] Host found on port: {} - port open".format(tcp_layer.sport)
+                    )
+            return True
+
         elif tcp_layer.flags.value == 0x14:
             if verbosity == True:
-                print("[+] Host found on port: {} - port closed".format(tcp_layer.sport))
-            found_host=True
-        else:
-            print ("unknown response retry scan")
-        
-        if found_host==True:
-            return True 
+                print(
+                "[+] Host found on port: {} - port closed".format(tcp_layer.sport)
+                )
+            return True
 
     return False 
 
